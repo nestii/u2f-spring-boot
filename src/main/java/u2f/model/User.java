07cbd623f8c34164370a6e8c8a9b1853f103c6db
Nestorowicz <ewa.nestorowicz@capgemini.com>
2017-09-29 09:20:35 +0200

    first working version

---
+++ src/main/java/u2f/model/User.java
@@ -0,0 +1,7 @@
+package u2f.model;
+
+/**
+ * Created by Ewa on 4/23/2017.
+ */
+public class User {
+}
---
+++ src/main/java/u2f/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package u2f.repository;
+
+/**
+ * Created by Ewa on 4/24/2017.
+ */
+public class UserRepository {
+}
